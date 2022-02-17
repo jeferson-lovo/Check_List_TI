46189b52c13610fb8a18c326bcf18e75bf92c844
jeferson-lovo <jefersonlovo2009@hotmail.com>
2022-02-16 21:23:24 -0400

    v 16/02/2022

---
+++ resources/views/index1.blade.php
@@ -0,0 +1,36 @@
+
+@extends('layout.principal')
+
+@section('main')
+
+<div class="container">
+  <div class="py-5 text-center">
+    <h2>Serviços Realizados</h2>
+  </div>
+  <div class="row">
+
+    <!-- Departamentos -->
+    <div class="col-md-4" >
+      <h3>Fabricante</h3>
+      {{-- TODO --}}
+    </div>
+
+    <!-- Marcas -->
+    <div class="col-md-4" >
+      <h3>Marcas</h3>
+      {{-- TODO --}}
+    </div>
+
+    <!-- Produtos -->
+    <div class="col-md-4" >
+      <h3>Serviços</h3>
+      {{-- TODO --}}
+    </div>
+
+  </div>
+</div>
+
+
+
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 use Illuminate\Support\Facades\Route;
 
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index1');
 });
